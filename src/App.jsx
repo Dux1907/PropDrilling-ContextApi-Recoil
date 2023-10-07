91dd8d9838529a7f8e99636af01a97b694164f27
Dux <kartikmadaan1907@gmail.com>
2023-10-07 19:31:39 +0530

    Recoil

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,67 @@
 import { Button } from "@mui/material";
 import { TextField } from "@mui/material";
-import {Grid} from "@mui/material";
-import { createContext, useContext, useState } from "react";
+import { Grid } from "@mui/material";
+import { atom, useRecoilValue, useSetRecoilState, RecoilRoot } from "recoil";
 
-const countContext = createContext()
 const App = () => {
   const style = {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    textAlign : 'center'
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    textAlign: "center",
   }
-  const [title, setTitle] = useState(0)
   return (
     <>
-      <countContext.Provider value={{
-        title: title,
-        setTitle : setTitle
-      }}>
-      <h1 style={{ textAlign : 'center'}}>Counter App</h1>
-    <Grid style={style} container spacing={2}>
-      <Grid item xs = {10} md = {2}>
-          <Increase  />
-      </Grid>
+    <RecoilRoot>
+      <h1 style={{ textAlign: "center" }}>Counter App</h1>
+      <Grid style={style} container spacing={2}>
         <Grid item xs={10} md={2}>
-          <TextField id="outlined-basic" label="Counter" variant="outlined" value={title}/> </Grid>
+          <Increase />
+        </Grid>
+        <Grid item xs={10} md={2}>
+          <CountComponent />
+        </Grid>
         <Grid item xs={10} md={2}>
           <Decrease />
-       </Grid>
         </Grid>
-        </countContext.Provider>
+      </Grid>
+      </RecoilRoot>
       </>
-  )
-}
+  );
+};
 
 const Increase = () => {
-  const {title,setTitle} = useContext(countContext)
+  const setTitle = useSetRecoilState(countState);
   return (
-    <Button variant="outlined" onClick={() => setTitle(title + 1)}>Increase Counter</Button>
-  )
-}
+    <Button variant="outlined" onClick={() => setTitle((X) => X + 1)}>
+      Increase Counter
+    </Button>
+  );
+};
 
 const Decrease = () => {
-  const {title,setTitle} = useContext(countContext)
+  const setTitle = useSetRecoilState(countState);
   return (
-    <Button variant="outlined" onClick={() => setTitle(title - 1)}>Decrease Counter</Button>
-  )
-}
+    <Button variant="outlined" onClick={() => setTitle((X) => X - 1)}>
+      Decrease Counter
+    </Button>
+  );
+};
 
-export default App
+const CountComponent = () => {
+   const count = useRecoilValue(countState);
+  return (
+    <TextField
+      id="outlined-basic"
+      label="Counter"
+      variant="outlined"
+      value={count}
+    />
+ 
+  );
+};
+const countState = atom({
+  key: "countState",
+  default: 0,
+});
+export default App;
